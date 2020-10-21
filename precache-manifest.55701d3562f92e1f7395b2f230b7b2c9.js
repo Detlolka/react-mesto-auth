dc6d13b5681852f7e246f5d1ae3f340bb5b00422
Detlolka <detlolka@yahoo.com>
2020-10-21 23:24:23 +0300

    Updates

---
+++ precache-manifest.55701d3562f92e1f7395b2f230b7b2c9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56efd2149c4d17ec3fec74326575f574",
+    "url": "/index.html"
+  },
+  {
+    "revision": "570dd8183cea59d4271e",
+    "url": "/static/css/main.441f81f6.chunk.css"
+  },
+  {
+    "revision": "6fb3c16ba636092389ca",
+    "url": "/static/js/2.3149f414.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3149f414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "570dd8183cea59d4271e",
+    "url": "/static/js/main.d0972446.chunk.js"
+  },
+  {
+    "revision": "c29e50cb60e7fb181703",
+    "url": "/static/js/runtime-main.20bf01e0.js"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "/static/media/AddButton.bdfb8ebb.svg"
+  },
+  {
+    "revision": "58afbe73d6547d1730a443f381c68d9e",
+    "url": "/static/media/EditButton.58afbe73.svg"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
+    "url": "/static/media/Inter-Medium.f6cf0a0b.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "c2d0901f14de58acae456bda23f80e8a",
+    "url": "/static/media/__close.c2d0901f.svg"
+  },
+  {
+    "revision": "efa6fb04512d4ea565aaa4109835b46d",
+    "url": "/static/media/delElem.efa6fb04.svg"
+  },
+  {
+    "revision": "4072126c596bd9485c6e9601f6d4d35d",
+    "url": "/static/media/failed_auth.4072126c.svg"
+  },
+  {
+    "revision": "9fdebb6b009c16af50fae58657e30a94",
+    "url": "/static/media/like_active.9fdebb6b.svg"
+  },
+  {
+    "revision": "74771c85e32e79c73b64ebd93ef1fbf2",
+    "url": "/static/media/likes.74771c85.svg"
+  },
+  {
+    "revision": "855a8c9865e79463f31e74f9abcb4438",
+    "url": "/static/media/logo_header.855a8c98.svg"
+  },
+  {
+    "revision": "34c4685038e8cf551566cb4ee96e20e5",
+    "url": "/static/media/profile_pen.34c46850.svg"
+  },
+  {
+    "revision": "e38b395c190826b3bf6d78baff1415a0",
+    "url": "/static/media/success_auth.e38b395c.svg"
+  }
+]);
