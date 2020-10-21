acaff0e0875e88641824c245b774e69b5bf27e8e
Detlolka <detlolka@yahoo.com>
2020-10-21 23:19:19 +0300

    fixed: перенастройка API

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,10 @@
 
 export const  configApi = {
-    baseUrl: "https://mesto.nomoreparties.co/v1/cohort-13",
-    headers: {
-        authorization: "3d90ed66-5470-4061-9f08-ef73936051ae",
+    baseUrl: "https://detlolka-m.students.nomoreparties.space/",
+    headers: {        
         "Content-Type": "application/json",
     }
   };
 
-  export const authUrl = 'https://auth.nomoreparties.co';
+  export const authUrl = 'https://detlolka-m.students.nomoreparties.space/';
   
