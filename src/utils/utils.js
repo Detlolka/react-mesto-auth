37d6dc4ded2316c82436919f8ebf27d21b5df2cb
Detlolka <detlolka@yahoo.com>
2020-10-21 23:54:23 +0300

    fixed: Исправлена ошибка со слешем

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,10 @@
 
 export const  configApi = {
-    baseUrl: "https://detlolka-m.students.nomoreparties.space/",
+    baseUrl: "https://detlolka-m.students.nomoreparties.space",
     headers: {        
         "Content-Type": "application/json",
     }
   };
 
-  export const authUrl = 'https://detlolka-m.students.nomoreparties.space/';
+  export const authUrl = 'https://detlolka-m.students.nomoreparties.space';
   
